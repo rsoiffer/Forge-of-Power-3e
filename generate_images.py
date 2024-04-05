ae13991614a9c9a403cd4bb45d322ce807efe36c
rsoiffer <rsoiffer@gmail.com>
2024-04-04 17:43:28 -0700

    Improved prompt, added turbo workflow

--- generate_images.py
+++ generate_images.py
@@ -56,7 +56,7 @@ def get_images(ws, prompt):
                         current_node = data["node"]
                         print("Received data for", prompt[data["node"]]["class_type"])
         else:
-            if current_node == "49":
+            if prompt[current_node]["class_type"] == "SaveImageWebsocket":
                 images_output = output_images.get(current_node, [])
                 images_output.append(out[8:])
                 output_images[current_node] = images_output
@@ -64,21 +64,34 @@ def get_images(ws, prompt):
     return output_images
 
 
-def process(filepath, image_prompt):
-    with open("workflow_api.json", "r") as file:
-        prompt_text = file.read()
+def process(filepath, image_prompt, turbo=False):
+    if turbo:
+        with open("workflow_api_turbo.json", "r") as file:
+            prompt_text = file.read()
 
-    prompt_api = json.loads(prompt_text)
+        prompt_api = json.loads(prompt_text)
 
-    # Set positive prompt
-    prompt_api["6"]["inputs"]["text"] = prompt_api["15"]["inputs"]["text"] = (
-        image_prompt
-    )
+        # Set positive prompt
+        prompt_api["6"]["inputs"]["text"] = image_prompt
 
-    # Set negative prompt
-    prompt_api["7"]["inputs"]["text"] = prompt_api["16"]["inputs"]["text"] = (
-        "nsfw, bad hands, text, watermark, low quality, person, human, man, woman, 1boy, 1girl"
-    )
+        # Set negative prompt
+        prompt_api["7"]["inputs"]["text"] = "nsfw, text, watermark"
+
+    else:
+        with open("workflow_api.json", "r") as file:
+            prompt_text = file.read()
+
+        prompt_api = json.loads(prompt_text)
+
+        # Set positive prompt
+        prompt_api["6"]["inputs"]["text"] = prompt_api["15"]["inputs"]["text"] = (
+            image_prompt
+        )
+
+        # Set negative prompt
+        prompt_api["7"]["inputs"]["text"] = prompt_api["16"]["inputs"]["text"] = (
+            "nsfw, text, watermark"
+        )
 
     ws = websocket.WebSocket()
     ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
@@ -108,29 +121,25 @@ def main():
                 if os.path.exists(filepath):
                     continue
 
-                print("Processing power:", name)
+                power_desc = name
+                power_desc += "\nType: " + class_name + ", " + entry["type"]
+                power_desc += "\nDescription: " + entry["brief"]
+                if "traits" in entry:
+                    power_desc += "\nTraits: " + ", ".join(entry["traits"])
+                if "tags" in entry:
+                    for tag_name in entry["tags"]:
+                        power_desc += "\n" + tag_name + ": " + entry["tags"][tag_name]
+                power_desc += "\nEffect: " + entry["effect"]
+                if "fluff" in entry:
+                    power_desc += "\nFluff: " + entry["fluff"]
+                print("Processing power:", power_desc)
 
-                prompt1 = "The following is an ability in a medieval fantasy tabletop rpg system. Describe an image that shows this ability in action. The image should NOT focus on a person. Describe the focus of the image, then describe the background of the image, then describe the emotions and tone of the image. The image should NOT focus on a person. Your description should be formatted as a single very short paragraph of natural english writing. Be brief, be concise, and only include details the viewer can see with their eyes. Respond only with the description, nothing else.\n\n"
-                prompt1 += (
-                    name
-                    + "\nType: "
-                    + class_name
-                    + ", "
-                    + entry["type"]
-                    + "\nDescription: "
-                    + entry["brief"]
-                    + "\nEffect: "
-                    + entry["effect"]
+                prompt1 = (
+                    "The following is an ability in a medieval fantasy tabletop rpg system. Describe an image that shows this ability in action. The image should NOT focus on a person. Describe the focus of the image, then describe the background of the image, then describe the emotions and tone of the image. The image should NOT focus on a person. Your description should be formatted as a single very short paragraph of natural english writing. Be brief, be concise, and only include details the viewer can see with their eyes. Respond only with the description, nothing else.\n\n"
+                    + power_desc
                 )
                 description = run_ollama(prompt1)
-                # print('Generated Description:', description)
-
-                # prompt2 = 'Write a image generator prompt that matches the following description. The prompt should be formatted as a comma-separated list of about a dozen tags, where each tag is something you can see in the image. Respond only with the prompt, nothing else.\n\n' + name + ' (' + class_name + ')' + '\n' + description
-                # image_prompt = run_ollama(prompt2)
-                image_prompt = (
-                    "D&D, anime style, digital art, high quality, landscape, wallpaper, distant camera. "
-                    + description
-                )
+                image_prompt = "D&D, anime style, digital art. " + description
                 print("Generated Prompt:", image_prompt)
 
                 process(filepath, image_prompt)
